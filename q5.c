56c33885d765871ad5472a6684d906d6f67418d8
deepak-jalandhra-214 <jalandhrad688@gmail.com>
2026-08-17 23:24:34 +0530

    add pratice

---
+++ q5.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+
+// int main(){
+//     int a,b;
+//     a=b=2;
+//     int c=b+a;
+//     int bitwise=b^c;
+//     printf("%d\n",bitwise);
+//     int power=pow(b,c);
+//     printf("%d\n",power);
+//     return 0;
+// }
+// int main(){
+//     printf("%d\n",10%3);
+//     return 0;
+// }
+// int main(){
+//     int a=1.9999;
+//     printf("%d",a);
+//     return 0;
+// }
+// int main(){
+//     printf("%d",2+7*2);
+//     return 0;
+// }
+int main(){
+    printf("%d",2*4/7*9);
+    return 0;
+}
