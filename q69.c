f45ae8bbc20fa81f6d45663736ede80116943740
deepak-jalandhra-214 <jalandhrad688@gmail.com>
2026-09-01 00:04:05 +0530

    add

---
+++ q69.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int age=22;
+    int _age=23;
+    int *ptr=&age;
+    int *_ptr=&_age;
+    printf("diff=%u\n",ptr-_ptr);
+    _ptr=&age;
+    printf("comparision=%u\n",ptr==_ptr);
+    return 0;
+}
