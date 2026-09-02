7803b52914bd5620c87c08de3f1a940d53807854
deepak-jalandhra-214 <jalandhrad688@gmail.com>
2026-09-03 01:11:33 +0530

    add

--- q76.c
+++ q76.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 void reverse(int *ptr,int n);
-
 int main(){
     int array[]={1,2,3,4,5};
     int *ptr=&array[0];
@@ -8,7 +7,13 @@ int main(){
     return 0;
 }
 void reverse(int *ptr,int n){
-    for(int i=n-1;i>=0;i--){
+    for(int i=0;i<n/2;i++){
+        int first=*(ptr+i);
+        int last=*(ptr+n-i-1);
+        *(ptr+i)=last;
+        *(ptr+n-i-1)=first;
+    }
+    for(int i=0;i<n;i++){
         printf("%d\n",*(ptr+i));
     }
 }
